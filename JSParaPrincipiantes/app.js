b5e880d3df6a4cf7caa994c7c7458741f8e7f05d
CristobalSantana <124050867@upq.edu.mx>
2025-09-11 12:27:30 -0600

    Funciones
    Como hacer funciones y llamarlas

--- JSParaPrincipiantes/app.js
+++ JSParaPrincipiantes/app.js
@@ -1,4 +1,6 @@
-let name = ["ryan", "joe", "john" ];
-for (let i = 0; i < name.length; i++) {
-    console.log(name[i]);
+function add(n1, n2) {
+    console.log(n1 + n2); 
 }
+
+add(3, 2);
+add(100, 300);
