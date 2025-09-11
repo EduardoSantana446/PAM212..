40fb8e965f3ef30973daa3dab2b99ed0a9c49ab3
CristobalSantana <124050867@upq.edu.mx>
2025-09-11 11:56:12 -0600

    Bucle While

--- JSParaPrincipiantes/app.js
+++ JSParaPrincipiantes/app.js
@@ -1,12 +1,7 @@
-let typeCard = "Deit Card";
 
-switch (typeCard) {
-    case "Deit Card":
-        console.log("Tarjeta de débito");
-        break;
-    case "Credit Card":
-        console.log("Tarjeta de crédito");
-        break;
-    default: 
-        console.log('No card');     
+let count = 0;
+
+while(count <= 50){
+    console.log(count);
+    count++;
 }
