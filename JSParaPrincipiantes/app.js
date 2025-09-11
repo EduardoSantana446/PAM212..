78335a88cebb1554c25febc1e77e9bd2476a22f1
CristobalSantana <124050867@upq.edu.mx>
2025-09-11 11:29:07 -0600

    Condicionales
    Sentencia IF con password

--- JSParaPrincipiantes/app.js
+++ JSParaPrincipiantes/app.js
@@ -1,5 +1,10 @@
-let numberOne = 100;
-let numberTwo = 500;
+let passwordBD = 'pepe123';
+let input = 'pepe123';
 
-let result = numberOne > numberTwo;
-console.log(result);
+let result = input == passwordBD;
+
+if (result == true) {
+    console.log('Login correcto');
+} else {
+    console.log('Login incorrecto');
+} 
