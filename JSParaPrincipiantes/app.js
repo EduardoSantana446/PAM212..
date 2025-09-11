da4a72655864c96accd192cf27a7fb984c37f6e1
CristobalSantana <124050867@upq.edu.mx>
2025-09-10 23:05:09 -0600

    Arrays y Objetos
    Creamos Los tipos de Arrays y como se pueden crear los objetos.

--- JSParaPrincipiantes/app.js
+++ JSParaPrincipiantes/app.js
@@ -9,3 +9,30 @@
 // boolean
 true
 false
+
+// Array
+['joe', 'ryan', 'martha']
+[1,2,3]
+[true, false, true, false]
+
+// Object
+'ryan'
+70.4
+14
+true
+
+{
+    "username": 'ryan',
+    "score": 70.4,
+    "hours": 14,
+    "proffesional": true
+    
+}
+
+{
+    "username": 'joe',
+    "score": 0.4,
+    "hours": 1,
+    "proffesional": false
+    
+}
