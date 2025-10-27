9eddc1fd156afa9bf7fd86d3415d17d96b01df84
CristobalSantana <124050867@upq.edu.mx>
2025-10-26 22:21:33 -0600

    Practica 11: Repaso (completada)

--- intro/screens/Repaso.js
+++ intro/screens/Repaso.js
@@ -1,5 +1,5 @@
-import React, { useEffect, useRef, useState } from "react";
-import {View, Text, Animated, StyleSheet, Dimensions, ImageBackground,} from "react-native";
+import React, { use, useEffect, useRef, useState } from "react";
+import {View, Text, Animated, StyleSheet, Dimensions, ImageBackground, TextInput, Alert, Button, Switch} from "react-native";
 import * as SplashScreen from "expo-splash-screen";
 
 SplashScreen.preventAutoHideAsync();
@@ -7,7 +7,31 @@ SplashScreen.preventAutoHideAsync();
 const { height } = Dimensions.get("window");
 
 export default function SplashScreenPro() {
+  const [encendido, setEncentido] = useState(false);
+  const [nombre, setNombre] = useState('');
+  const [correo, setCorreo] = useState('');
   const [showMain, setShowMain] = useState(false); // controla la pantalla principal
+
+  const enviarDatos = () => {
+      if (nombre.trim() === '') {
+        Alert.alert('Error', 'nombre no ingresado');
+        alert('Error: nombre no ingresado');
+      } else if (correo.trim() === '') {
+        Alert.alert('Error', 'correo no ingresado');
+        alert('Error: correo no ingresado');
+      } else if (!correo.includes('@gmail.com')) {
+        Alert.alert('Error', 'correo no valido');
+        alert('Error: correo no valido');
+      } else if (!encendido) {
+        Alert.alert('Error', 'Debes aceptar los terminos y condiciones');
+        alert('Error: Debes aceptar los terminos y condiciones');
+      } else {
+        Alert.alert('Exito', 'Datos enviados correctamente');
+        alert('Exito: Datos enviados correctamente');
+      }
+    };
+
+    
 
   // Animaciones Splash
   const fadeLogo = useRef(new Animated.Value(0)).current;
@@ -15,6 +39,7 @@ export default function SplashScreenPro() {
   const rotateLogo = useRef(new Animated.Value(0)).current;
   const slideText = useRef(new Animated.Value(height / 2)).current;
   const fadeOut = useRef(new Animated.Value(1)).current;
+
 
   useEffect(() => {
     // Animación inicial del logo: fade + scale + rotación
@@ -67,22 +92,48 @@ export default function SplashScreenPro() {
   if (showMain) {
     return (
       <ImageBackground
-        source={require("../assets/SperMario.webp")}
+        source={require("../assets/FondoCOD.jpeg")}
         style={styles.background}
         resizeMode="cover" // 'cover' hace que la imagen llene toda la pantalla
       >
         <View style={styles.content}>
-          <Text style={styles.text}>¡Bienvenido!</Text>
+            <Text style={styles.title}> Registro de usuario </Text>
+
+            <TextInput
+                style={styles.input}
+                placeholder="Nombre completo"
+                value={nombre}
+                onChangeText={setNombre}
+            />
+
+            <TextInput
+                style={styles.input}
+                placeholder="Correo electrónico"
+                value={correo}
+                onChangeText={setCorreo}
+            />
+
+            <View style={styles.switchContainer}>
+              <Text>¿Aceptas los términos y condiciones?</Text>
+              <Switch value={encendido} onValueChange={setEncentido}/>
+            </View>
+
+            <Button title="Enviar datos" onPress={enviarDatos} />
         </View>
+
       </ImageBackground>
+
     );
   }
+
+
+
 
   // Splash animado
   return (
     <Animated.View style={[styles.container, { opacity: fadeOut }]}>
       <Animated.Image
-        source={require("../assets/NALGON.png")}
+        source={require("../assets/IconoProgreso.jpg")}
         resizeMode="contain"
         style={[
           styles.logoImage,
@@ -95,7 +146,7 @@ export default function SplashScreenPro() {
       <Animated.Text
         style={[styles.text, { transform: [{ translateY: slideText }] }]}
       >
-        ¡ImageBackground & Splash Screen!
+        Cargando aplicación...
       </Animated.Text>
       <Animated.View
         style={[
@@ -120,7 +171,7 @@ export default function SplashScreenPro() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#007bffff",
+    backgroundColor: "#5e6f82ff",
     justifyContent: "center",
     alignItems: "center",
   },
@@ -141,11 +192,14 @@ const styles = StyleSheet.create({
     height: "100%", // alto completo
     justifyContent: "center",
     alignItems: "center",
+
   },
   content: {
-    backgroundColor: "rgba(0, 0, 0, 0.5)",
+    backgroundColor: "rgba(255, 255, 255, 1)",
     padding: 20,
     borderRadius: 10,
+    gap: 10,
+    alignItems: "center"
   },
   text: {
     color: "white",
@@ -153,4 +207,24 @@ const styles = StyleSheet.create({
     marginBottom: 10,
     textAlign: "center",
   },
+
+  title:{
+    fontSize:40,
+    fontWeight:'bold'
+  },
+
+  input:{
+    width:'100%',
+    borderWidth:1,
+    borderColor:'#86828fff',
+    padding:10,
+    borderRadius:10,
+
+  },
+
+  switchContainer:{
+    flexDirection:'row',
+    gap:10,
+  },
+
 });
